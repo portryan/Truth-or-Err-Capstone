117228013faf9a6c5dca4b8d037dc2446d1994e9
Ryan Porter <porter.ryan42@gmail.com>
2023-02-17 16:23:59 -0500

    Add hello world button test

--- app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
+++ app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
@@ -3,6 +3,9 @@ package com.portryan.truthorerrcapstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        TextView TV1 = (TextView) findViewById(R.id.textView1);
+        TextView TV2 = (TextView) findViewById(R.id.textView2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String text1 = TV1.getText().toString();
+                String text2 = TV2.getText().toString();
+
+                TV1.setText(text2);
+                TV2.setText(text1);
+
+
+            }
+        });
     }
 }
