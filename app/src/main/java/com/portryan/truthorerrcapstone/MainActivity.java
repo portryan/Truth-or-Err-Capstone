69f75de8cb5fa4d0918984a434a2fc7db717e698
Ryan Porter <porter.ryan42@gmail.com>
2023-03-17 14:20:09 -0400

    Added Activities

--- app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
+++ app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
@@ -2,9 +2,11 @@ package com.portryan.truthorerrcapstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,19 +16,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        TextView TV1 = (TextView) findViewById(R.id.textView1);
-        TextView TV2 = (TextView) findViewById(R.id.textView2);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String text1 = TV1.getText().toString();
-                String text2 = TV2.getText().toString();
+        EditText searchText = (EditText)findViewById(R.id.searchText);
+        Button createQuestionBtn = (Button)findViewById(R.id.createQuestionBtn);
+        Button answerQuestionBtn = (Button)findViewById(R.id.answerQuestionBtn);
+        Button createCategoryBtn = (Button)findViewById(R.id.createCategoryBtn);
+        Button categoriesBtn = (Button)findViewById(R.id.categoriesBtn);
 
-                TV1.setText(text2);
-                TV2.setText(text1);
-
-
+        createQuestionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateQuestion.class);
+                startActivity(intent);
             }
         });
+
+        answerQuestionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnswerQuestion.class);
+                startActivity(intent);
+            }
+        });
+
+        createCategoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateCategory.class);
+                startActivity(intent);
+            }
+        });
+
+        categoriesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Categories.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
