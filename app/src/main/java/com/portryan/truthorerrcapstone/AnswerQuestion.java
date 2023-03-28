0be64a6eefe6b5e94ad2f16a25514b40026b8273
Ryan Porter <porter.ryan42@gmail.com>
2023-03-27 20:16:29 -0400

    Added options menu

--- app/src/main/java/com/portryan/truthorerrcapstone/AnswerQuestion.java
+++ app/src/main/java/com/portryan/truthorerrcapstone/AnswerQuestion.java
@@ -3,8 +3,17 @@ package com.portryan.truthorerrcapstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class AnswerQuestion extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
+++ app/src/main/java/com/portryan/truthorerrcapstone/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +14,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
