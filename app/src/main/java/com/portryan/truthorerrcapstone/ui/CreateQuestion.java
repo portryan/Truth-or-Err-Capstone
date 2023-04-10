42b92afc3bc93d373164caa46106e93d5a9067ad
Ryan Porter <porter.ryan42@gmail.com>
2023-04-10 19:06:59 -0400

    Added create question page (not working)

--- app/src/main/java/com/portryan/truthorerrcapstone/ui/CreateQuestion.java
+++ app/src/main/java/com/portryan/truthorerrcapstone/ui/CreateQuestion.java
@@ -8,10 +8,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.portryan.truthorerrcapstone.R;
 
-public class CreateQuestion extends AppCompatActivity {
+public class CreateQuestion extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,5 +49,20 @@ public class CreateQuestion extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setTitle("Create Question");
+
+        Spinner correctAnswerSpinner = (Spinner)findViewById(R.id.correct_dropdown);
+        String[] ansNums = getResources().getStringArray(R.array.correctAnswerNumbers);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, ansNums);
+        correctAnswerSpinner.setPrompt("TEst");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
